39d41923f82f2495225e560167e2b356475370c2
Arshad Mehmood <arshad115@hotmail.com>
2020-07-17 16:57:22 +0200

    Day 1 - Top K Frequent Elements

---
+++ codes/2020-07-17-top-k-frequent-elements.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        mostFrequent = {}
+
+        for x in nums:
+            if x in mostFrequent:
+                mostFrequent[x] += 1
+            else:
+                mostFrequent[x] = 1
+
+        # Sort list in descending order to get most frequent on top
+        mostFrequent = {k:v for k, v in sorted(mostFrequent.items(), key= lambda x: x[1], reverse=True)}
+
+        return list(mostFrequent.keys())[:k] # slicing top k
+
+
+nums = [4,1,-1,2,-1,2,3]
+k = 2
+
+solution = Solution()
+print(solution.topKFrequent(nums,k))
