ec26015deee9e5202f694ca9b412ee4fd6722503
Arshad Mehmood <arshad115@hotmail.com>
2020-07-24 23:02:50 +0200

    Day 8 - Valid Number

---
+++ codes/2020-07-24-valid-number.py
@@ -0,0 +1,30 @@
+# -------------------------------------------------------
+# Valid Number - https://leetcode.com/problems/valid-number/
+# -------------------------------------------------------
+# Author: Arshad Mehmood
+# Github: https://github.com/arshad115
+# Blog: https://arshadmehmood.com
+# LinkedIn: https://www.linkedin.com/in/arshadmehmood115
+# Date : 2020-07-23
+# Project: 100-days-of-leetcode
+# -------------------------------------------------------
+import re
+import sys
+from math import inf
+from statistics import median
+from typing import List
+
+class Solution:
+    def isNumberUsingExceptionHandling(self, s: str) -> bool:
+        try:
+            f = float(s)
+            return True
+        except:
+            return False
+    def isNumber(self, s: str) -> bool:
+        reg = re.compile("^[+-]?((\d+\.?\d*)|(\d*\.\d+))((e[+-]?\d+$)|$)")
+        return reg.match(s.strip())
+
+solution = Solution()
+num  = solution.isNumber("10e5")
+print(num)
